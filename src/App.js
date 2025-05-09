a40459c67c28dba1feb2bfe169e18c17be91dafd
Aahad <aahadqureshi786@gmail.com>
2025-05-09 16:40:53 +0530

    kclub edits color

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function App() {
   return (
     <>
     <style>{`
-    .nav-text:hover{
+    .nav-text{
             color: #5846f9 !important;
     }
       .text-box {
@@ -98,12 +98,12 @@ function App() {
     `}</style>
     <div className="App">
       {/* <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav"> */}
-      <nav className={color ? 'navbar navbar-expand-lg navbar-dark fixed-top bg-dark' : 'navbar navbar-expand-lg navbar-dark fixed-top'} id="mainNav">
+      <nav className={color ? 'navbar navbar-expand-lg navbar-dark fixed-top bg-white' : 'navbar navbar-expand-lg navbar-dark fixed-top bg-white'} id="mainNav">
     <div className="container">
       <a className="navbar-brand js-scroll-trigger" href="#page-top">
 <img src={logo} style={{width:150}} />
       </a>
-      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler text-back navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i className="fa fa-bars"></i>
       </button>
